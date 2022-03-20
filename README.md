e3a0ceccd54441ef0fbe5afc3f6acf08442bf2f6
Chris Bain <baincd@users.noreply.github.com>
2022-03-20 12:22:58 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Java Class Imports Folding
+
+Add folding for imports on Java class files.
+
+## Features
+
+Adds a folding region for imports on Java \*.class files.
+
+This is a workaround for [vscode-java#2133 "import regions not computed for .class files"](https://github.com/redhat-developer/vscode-java/issues/2133)
+
+## Extension Settings
+
+None.
+
---
+++ src/extension.ts
@@ -0,0 +1,48 @@
+import * as vscode from 'vscode';
+
+class DiffFoldingRangeProvider implements vscode.FoldingRangeProvider {
+    onDidChangeFoldingRanges?: vscode.Event<void> | undefined;
+
+    private readonly importLineRegEx = /^\s*import\s.*$/
+    private readonly whitespaceLineRegEx = /^\s*$/
+
+    
+    provideFoldingRanges(document: vscode.TextDocument, context: vscode.FoldingContext, token: vscode.CancellationToken): vscode.ProviderResult<vscode.FoldingRange[]> {
+        let firstImport = -1;
+        let lastImport = -1;
+
+        for (let i = 0; i < document.lineCount; i++) {
+            if (this.importLineRegEx.test(document.lineAt(i).text)) {
+                firstImport = i;
+                break;
+            }
+        }
+
+        if (firstImport < 0) {
+            return [];
+        }
+
+        for (let i = firstImport + 1; i < document.lineCount; i++) {
+            if (this.importLineRegEx.test(document.lineAt(i).text)) {
+                lastImport = i;
+            } else if (!this.whitespaceLineRegEx.test(document.lineAt(i).text)) {
+                break;
+            }
+        }
+
+        if (lastImport < 0) {
+            return [];
+        }
+        
+        return [new vscode.FoldingRange(firstImport, lastImport, vscode.FoldingRangeKind.Imports)];
+    }
+
+}
+
+
+export function activate(context: vscode.ExtensionContext) {
+    context.subscriptions.push(
+        vscode.languages.registerFoldingRangeProvider(
+            {language: "java", pattern: "**/*.class"}, new DiffFoldingRangeProvider()));
+
+}
