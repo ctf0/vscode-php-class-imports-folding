c52b48ca5bf388eaedea277c4eba0c26601f5726
Chris Bain <baincd@users.noreply.github.com>
2022-11-12 16:11:17 -0500

    Mark java-class-imports-folding extension obsolete
    Extension is obsolete with redhat-developer/vscode-java#2133 resolved.

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# Java Class Imports Folding
+# Java Class Imports Folding [OBSOLETE]
+
+This extension is obsolete.
+
+This extension was a workaround for [vscode-java#2133](https://github.com/redhat-developer/vscode-java/issues/2133), which is fixed in [Language Support for Java(TM) by Red Hat v1.12](https://github.com/redhat-developer/vscode-java/releases/tag/v1.12.0).
+
+---
+# Original README
 
 Add folding for imports on Java class files.
 
--- src/extension.ts
+++ src/extension.ts
@@ -41,8 +41,33 @@ class DiffFoldingRangeProvider implements vscode.FoldingRangeProvider {
 
 
 export function activate(context: vscode.ExtensionContext) {
+    if (!vscode.workspace.getConfiguration().get<boolean>("java-class-imports-folding.disableJavaExtensionVersionCheck")) {
+        javaExtVersionCheck();
+    }
+
     context.subscriptions.push(
         vscode.languages.registerFoldingRangeProvider(
             {language: "java", pattern: "**/*.class"}, new DiffFoldingRangeProvider()));
 
 }
+function javaExtVersionCheck() {
+    const javaExtFullVer = vscode.extensions.getExtension("redhat.java")?.packageJSON?.version as string;
+    if (javaExtFullVer) {
+        const [, major, minor] = javaExtFullVer.match(/^(\d+)\.(\d+)\./) || ["0.0", "0", "0"];
+        if (+major > 1 || +major == 1 && +minor >= 12) {
+            const MORE_INFO_BTN = "More Info";
+            const VIEW_EXT_SIDEBAR_BTN = "View in Extension Side Bar";
+
+            vscode.window.showWarningMessage(
+                "The extension \"Java Class Imports Folding\" is obsolete and can be uninstalled.",
+                VIEW_EXT_SIDEBAR_BTN, MORE_INFO_BTN
+            ).then(selection => {
+                switch (selection) {
+                    case MORE_INFO_BTN: vscode.commands.executeCommand('vscode.open', vscode.Uri.parse('https://github.com/baincd/vscode-java-class-imports-folding/blob/main/README.md')); break;
+                    case VIEW_EXT_SIDEBAR_BTN: vscode.commands.executeCommand("workbench.extensions.search", "baincd.java-class-imports-folding"); break;
+                }
+            });
+        }
+    }
+}
+
