c24a5a4bce69277f89426f0bbffa5e400fc12ec0
ctf0 <muah003@gmail.com>
2024-04-06 14:04:04 +0200

    003

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,7 @@
 ## 0.0.1
 
 - Init
+
+## 0.0.3
+
+- fix https://github.com/ctf0/vscode-php-class-imports-folding/issues/1
--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,30 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(
-        vscode.languages.registerFoldingRangeProvider('php', new FoldingProvider())
+        vscode.languages.registerFoldingRangeProvider('php', new FoldingProvider()),
     );
 }
 
 class FoldingProvider implements vscode.FoldingRangeProvider {
     provideFoldingRanges(document: vscode.TextDocument): vscode.ProviderResult<vscode.FoldingRange[]> {
-        const regEx = /^(\/\/)?\s*use\s.*$/g
+        const foldUse: any = this.foldUse(document);
+        const ranges: any = [];
+
+        if (!Array.isArray(foldUse)) {
+            ranges.push(
+                new vscode.FoldingRange(
+                    foldUse.firstImport,
+                    foldUse.lastImport,
+                    vscode.FoldingRangeKind.Imports,
+                ),
+            );
+        }
+
+        return ranges;
+    }
+
+    foldUse(document: vscode.TextDocument): [] | { firstImport: number; lastImport: number; } {
+        const regEx = /^(\/\/( )?)?use .*$/g;
         let firstImport = -1;
         let lastImport = -1;
 
@@ -29,12 +46,6 @@ class FoldingProvider implements vscode.FoldingRangeProvider {
             }
         }
 
-        return [
-            new vscode.FoldingRange(
-                firstImport,
-                lastImport,
-                vscode.FoldingRangeKind.Imports
-            )
-        ];
+        return { firstImport, lastImport };
     }
 }
